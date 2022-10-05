377536bdd8bf8ab6fac794409ffaa488524cfc07
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-10-05 15:31:18 +0530

    Add:Program to find sum using command line

---
+++ SumUsingComandLine.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class SumUsingComandLine {
+	public static void main(String args[])   
+	{  
+	int number1 = 12, number2 = 10, sum;  
+	sum = number1 + number2;  
+	System.out.println("The sum of numbers is: "+sum);  
+	}  
+
+}
