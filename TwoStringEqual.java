4bd58349a98e33cba2f4afa3222eea87ce470428
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-10-05 15:33:00 +0530

    Add:Java program to check two strings are equal or not

---
+++ TwoStringEqual.java
@@ -0,0 +1,14 @@
+package Day1;
+
+public class TwoStringEqual {
+	public static void main(String[] args) {
+
+		String name1 = "Imran";
+		String name2 = "Irfan";
+
+		if (name1 == name2)
+			System.out.println("String are Equal");
+		else
+			System.out.println("String are Not Equal");
+	}
+}
